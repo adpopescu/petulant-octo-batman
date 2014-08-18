d77c87daa7ad8cb0cb4c88555c872dd539dede0d
Adrian Popescu <adpopescu@gmail.com>
2014-08-18 18:07:32 -0400

    Console outputs text

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 /**
  * Created by dapopesc on 18/08/14.
  */
-var main  = function() {
+var main = function () {
     "use strict";
 
+    $(".comment-input button").on("click", function (event) {
+        console.log("Hello World!");
+    });
 };
 
 $(document).ready(main);
