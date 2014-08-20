031a350dbf9e8093b5ff9cb37bc9daf6183a3ea8
Adrian Popescu <adpopescu@gmail.com>
2014-08-20 18:12:46 -0400

    moved files into substructure

--- javascripts/app.js
+++ javascripts/app.js
@@ -4,26 +4,25 @@
 var main = function () {
     "use strict";
 
-    $(".comment-input button").on("click", function (event) {
+    var addCommentFromINputBox = function () {
         var $new_comment;
 
         if ($(".comment-input input").val() !== "") {
             $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment.hide();
             $(".comments").append($new_comment);
+            $new_comment.fadeIn();
             $(".comment-input input").val("");
         }
+    }
+
+    $(".comment-input button").on("click", function (event) {
+        addCommentFromINputBox()
     });
 
     $(".comment-input input").on("keypress", function(event) {
-        var $new_comment;
-        if (event.keyCode === 13){
-            if ($(".comment-input input").val() !== "") {
-                $new_comment = $("<p>").text($(".comment-input input").val());
-                $new_comment.hide();
-                $(".comments").append($new_comment);
-                $new_comment.fadeIn();
-                $(".comment-input input").val("");
-            }
+        if (event.keyCode === 13) {
+            addCommentFromINputBox()
         }
     });
 };
