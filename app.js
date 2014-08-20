2836f8af1202d905da68d0f78576e799b3d1304e
Adrian Popescu <adpopescu@gmail.com>
2014-08-20 17:23:24 -0400

    Fades in the text. Enter key also works now.

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ var main = function () {
         if ($(".comment-input input").val() !== "") {
             $new_comment = $("<p>").text($(".comment-input input").val());
             $(".comments").append($new_comment);
+            $(".comment-input input").val("");
+        }
+    });
+
+    $(".comment-input input").on("keypress", function(event) {
+        var $new_comment;
+        if (event.keyCode === 13){
+            if ($(".comment-input input").val() !== "") {
+                $new_comment = $("<p>").text($(".comment-input input").val());
+                $new_comment.hide();
+                $(".comments").append($new_comment);
+                $new_comment.fadeIn();
+                $(".comment-input input").val("");
+            }
         }
     });
 };
